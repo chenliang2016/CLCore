eef543dfd2ffeeba7b0bddb9ddc7df0606be0a1b
chenliang <appleidofcl@163.com>
2017-04-11 23:57:54 +0800

    add

---
+++ CLCore/CLCore.h
@@ -0,0 +1,15 @@
+//
+//  RouterConfig.swift
+//  CLIOSFrame
+//
+//  Created by chenliang on 2017/1/1.
+//  Copyright © 2017年 chenliang. All rights reserved.
+//
+
+
+@import Foundation;
+
+FOUNDATION_EXPORT double CLCoreVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char CLCoreVersionString[];
+
